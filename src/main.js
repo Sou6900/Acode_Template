aa17bf3e12ac5aad232877d4460291a56e68c611
Sou6900 <103485492+Sou6900@users.noreply.github.com>
2025-04-03 22:46:51 +0530

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import plugin from '../plugin.json';
+//this is the main acode class
+class AcodePlugin {
+//acode initinalization
+  async init() { 
+    //your code 
+    window.toast('Hello world');
+  }    
+ 
+  async destroy() {
+  //what happens on destroy
+  }
+}
+//the geature before we load the plugin
+if (window.acode) {
+  const acodePlugin = new AcodePlugin();
+  acode.setPluginInit(plugin.id, async (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
+    if (!baseUrl.endsWith('/')) {
+      baseUrl += '/';
+    }
+    acodePlugin.baseUrl = baseUrl;
+    await acodePlugin.init($page, cacheFile, cacheFileUrl);
+  });
+  acode.setPluginUnmount(plugin.id, () => {
+    acodePlugin.destroy();
+  });
+}
