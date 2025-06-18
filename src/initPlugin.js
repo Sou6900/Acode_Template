cee6b80a1fd2d9a6216a15c5f9821c41118613c5
dev.sourav <103485492+Sou6900@users.noreply.github.com>
2025-06-18 11:54:06 +0530

    Update initPlugin.js

--- src/initPlugin.js
+++ src/initPlugin.js
@@ -1,1 +1,18 @@
 
+import { getSettings, settingsList, onSettingsChange } from './settings.js';
+
+let __SETTINGS = null;
+
+export function initPlugin() {
+  const editor = editorManager.editor;
+
+ //  // setting usage 
+ // __SETTINGS = getSettings();
+ //  if(__SETTINGS.myplugin_settings01 == true){
+ //    return ;
+ //  }
+
+  // // event example : cursor & selection change
+  // editor.on("changeSelection", safeClickHandler);
+  // editor.selection.on("changeCursor", safeClickHandler);
+}
