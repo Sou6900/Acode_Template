e627e1450d17a236817bc915b059e78cf3a208ce
dev.sourav <103485492+Sou6900@users.noreply.github.com>
2025-06-18 11:35:53 +0530

    Update _main.js

--- src/_main.js
+++ src/_main.js
@@ -1,1 +1,29 @@
 
+import plugin from '../plugin.json';
+import { initPlugin } from './core/initPlugin.js';
+import './style.css';
+import { getSettings, settingsList, onSettingsChange } from './settings.js';
+
+// let __SETTINGS = null;
+
+// __SETTINGS = getSettings();
+//   if(__SETTINGS.myplugin_setting1 == true){
+//     return ;
+//   }
+
+let baseUrl = '';
+
+function destroyPlugin() {
+  // cleanup logic
+}
+
+if (window.acode) {
+  acode.setPluginInit(plugin.id, async (_baseUrl, $page, { cacheFileUrl, cacheFile }) => {
+    await initPlugin($page, cacheFile, cacheFileUrl);
+  }, {
+    list: settingsList,
+    cb: onSettingsChange
+  });
+
+  acode.setPluginUnmount(plugin.id, () => destroy());
+}
